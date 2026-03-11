31ea61b6ef9b14a70072002d734f404bdf0d96e6
serendipity-sketch <2432160242@qq.com>
2026-03-11 20:27:52 +0800

    更新

--- c++/vector.cpp
+++ c++/vector.cpp
@@ -4,6 +4,16 @@ using namespace std;
 #include<algorithm>
 int main(){
     vector<int>v;
-    
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(2);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+
+    }
+    cout<<endl;
+//查找函数，最先出现元素的位置find(),他返回的是个指针
+    auto it =find(v.begin(),v.end(),2)-v.begin();
+    cout<<it;
     return 0;
 }

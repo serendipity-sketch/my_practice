e4a774d328227a4cc2894c5fe6d532d4a4b6bd78
serendipity-sketch <2432160242@qq.com>
2026-03-16 20:34:09 +0800

    更新

--- c++/deque.cpp
+++ c++/deque.cpp
@@ -1,38 +1,29 @@
-// #include <iostream>
-// using namespace std;
-// #include<deque>
-// #include<vector>
-// int main(){
-//     deque<int>de;
- 
-
-//     return 0;
-// }
-
-
 #include <iostream>
 using namespace std;
 #include<deque>
 #include<vector>
-int main(){
-    //deque<int>de;
-    vector<int>v{10,54,6,1,3,45};
-    for(int i=0;i<v.size();i++){
-        int min=i;
-        for(int j=i+1;j<v.size();j++){
-            if(v[min]>v[j]){
-                min=j;
-            }
-        }
-        if(min!=i){
-            int temp=v[min];
-            v[min]=v[i];
-            v[i]=temp;
-        }
-    }
-    for(auto x:v){
+void Print(deque<int>de){
+    for(auto x:de){
         cout<<x<<" ";
     }
+    cout<<endl;
+
+}
+ void test(){
+    deque<int>de;
+    for(int i=0;i<10;i++){
+        de.push_back(i);
+    }
+    Print(de);
+    de.insert(de.begin(),2,4);
+    Print(de);
+
+
+
+}
+int main(){
+    
+    test();
 
     return 0;
 }
--- c++/luogu.cpp
+++ c++/luogu.cpp
@@ -1,18 +1,52 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//   //P1307 [NOIP 2011 普及组] 数字反转
+//     int N;
+//     cin>>N;
+//     if(N ==0){
+//       cout<<0;
+//   }else{
+//        int a=0;
+//        while(N){
+//         a=a*10+N%10;
+//         N = N/10;   
+//     }
+//        cout<<a;
+//   }
+//   return 0;
+// }
+
+
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
-  //P1307 [NOIP 2011 普及组] 数字反转
-    int N;
-    cin>>N;
-    if(N ==0){
-      cout<<0;
-  }else{
-       int a=0;
-       while(N){
-        a=a*10+N%10;
-        N = N/10;   
+    int k;
+    cin>>k;
+    vector<int>v(k);
+    for(int i=0;i<k;i++){
+        cin>>v[i];
     }
-       cout<<a;
-  }
-  return 0;
+    int one=-1;
+    int five=-1;
+    int ten=-1;
+    for(int i=0;i<k;i++){
+        if(v[i]==1&&one==-1){
+            one=i;
+        }
+  
+        if(v[i]==5&&five==-1){
+            five=i;
+        }
+        if(v[i]==10&&ten==-1){
+            ten=i;
+        }
+    }
+    if(one==-1||five==-1||ten==-1){
+      cout<<"False";
+    }else{
+      cout<<one<<" "<<five<<" "<<ten;
+    }
+    return 0;
 }
